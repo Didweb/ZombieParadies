fcd69f0e439c6353675d4bfa7fedb1a1edab2c59
Edu <info@did-web.com>
2016-11-14 22:29:59 +0100

    Lectura de Tils para suelo creacion de suelo

--- src/factoryTile/RecMapa.java
+++ src/factoryTile/RecMapa.java
@@ -2,7 +2,9 @@ package factoryTile;
 
 
 
+import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
@@ -25,6 +27,14 @@ public class RecMapa {
  * 
  */
 	
+	private static BufferedImage imagen;
+	private static BufferedImage imagenBruto;
+
+	private static BufferedImage recorte;
+	
+	
+	public static Image[][] suelo;
+	
 	public static int[] MapTile; // Resultado en un array los codigos de los tiles.
 	private static  int[] planoBruto;
 	
@@ -45,10 +55,11 @@ public class RecMapa {
 	
 	public static void DataPlanos(int nivel){
 		
+		
 		cargarImg("recursos/plano_"+nivel+".gif");
 		
+		
 		MapTile = new int[MapTileN];
-		planoBruto = new int[MapTileN];
 		
 		for(int i=0; i<MapTileN; i++){
 			
@@ -66,15 +77,54 @@ public class RecMapa {
 					
 					
 					default:
-						MapTile[i] = 0;
+						MapTile[i] = 2;
 					}
 			
 		}
 		
+		pintarSuelo(nivel);
 
 		
 		NivelCargado = nivel;
 		
+		
+		
+	}
+	
+	
+	private static void pintarSuelo(int nivel){
+		
+		suelo = new Image [MapTileN][MapTileN];
+		
+		 File f = new File("recursos/Tiles_Suelo_"+nivel+".gif");
+		    try {
+		    	imagen = ImageIO.read(f);
+				} catch (IOException e) {
+				    e.printStackTrace();
+				}
+	    
+
+	    		int lax=0;
+	    		int lay=0;
+	    		int posX;
+	    		int posY;
+	    		
+				for(int x=0;x<MapTileN;x++){
+					
+					if( lax == WidthMap ){
+						lax = 0;
+						lay += 1;
+						} else {
+							lax += 1; }
+					
+					posX = MapTile[x]*ANCHO_TILE;
+					posY = ((int)(Math.random() * 5)+1)*ANCHO_TILE;
+
+					recorte = (imagen.getSubimage(posX, posY, ANCHO_TILE, ANCHO_TILE));
+				            
+					suelo[lax][lay] = recorte; 
+
+				}
 		
 		
 	}
@@ -92,15 +142,19 @@ public class RecMapa {
 			AnchoPantallaMap = WidthMap * ANCHO_TILE;
 			AltoPantallaMap = HightMap * ANCHO_TILE;
 			
-			
+			planoBruto = new int[MapTileN];
 			imagen.getRGB(0, 0, WidthMap, HightMap, planoBruto, 0, WidthMap);
 			
 			} catch (IOException e) {
 				System.out.println(" Imagen no encontrada ");
 				e.printStackTrace();
+				planoBruto = new int[MapTileN];
 			}
+		
+		
 		
 	}
 	
+
 	
 }
--- src/zombieParadies/Pantalla.java
+++ src/zombieParadies/Pantalla.java
@@ -34,6 +34,20 @@ public class Pantalla extends JPanel {
 
 	public void paint(Graphics g){
 		
+		int lax=0;
+		int lay=0;
+		for(int x=0; x < RecMapa.MapTileN; x++){
+			if( lax == RecMapa.WidthMap ){
+				lax = 0;
+				lay += 1;
+				} else {
+					lax += 1; }
+			
+			g.drawImage(RecMapa.suelo[lax][lay], lax*RecMapa.ANCHO_TILE, lay*RecMapa.ANCHO_TILE, this);
+		}
+		
+		
+		
 		// Mostrar o no mostrar alertas de dev
 		if (Juego.isVerDev()){
 			mostrarDev(g); }
